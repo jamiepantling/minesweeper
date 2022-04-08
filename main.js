8252a0328593ba10e23fa9a64cbbb26a57aa709c
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-04-08 13:05:42 -0400

    add grid init function

---
+++ main.js
@@ -0,0 +1,29 @@
+let grid = [];
+function init(){
+    grid  = [
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0]
+    ];
+    grid.forEach(function(row, rowNumber) {
+        row.forEach(function(square, columnNumber) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("square");
+            squareDiv.id = `x-${rowNumber}-y-${columnNumber}`
+            const gridContainerEl = document.getElementById("grid-container");
+            gridContainerEl.appendChild(squareDiv);
+        }
+        )
+    }
+    )
+}
+init()
