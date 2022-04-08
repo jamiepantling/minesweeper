c6d3fb1c315b560e8f79f2907a5215377455d10d
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-04-08 14:33:39 -0400

    add eventlisteners

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
 let grid = [];
-function init(){
-    grid  = [
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0]
-    ];
-    grid.forEach(function(row, rowNumber) {
-        row.forEach(function(square, columnNumber) {
-            const squareDiv = document.createElement("div");
-            squareDiv.classList.add("square");
-            squareDiv.id = `x-${rowNumber}-y-${columnNumber}`
-            const gridContainerEl = document.getElementById("grid-container");
-            gridContainerEl.appendChild(squareDiv);
-        }
-        )
-    }
-    )
+function init() {
+  grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  grid.forEach(function (row, rowNumber) {
+    row.forEach(function (square, columnNumber) {
+      const squareDiv = document.createElement("div");
+      squareDiv.classList.add("square");
+      squareDiv.id = `${rowNumber},${columnNumber}`;
+      const gridContainerEl = document.getElementById("grid-container");
+      gridContainerEl.appendChild(squareDiv);
+    });
+  });
 }
-init()
+document
+  .getElementById("grid-container")
+  .addEventListener("click", clickHandler);
+document
+  .getElementById("grid-container")
+  .addEventListener("contextmenu", rightClickHandler);
+  
+init();
+
+function clickHandler(event) {
+  let coords = event.target.id;
+  console.log("Clicked " + coords);
+}
+function rightClickHandler(event) {
+  let coords = event.target.id;
+  console.log("Right clicked " + coords);
+}
